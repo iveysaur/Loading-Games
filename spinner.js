05cd899db1ed4b6baf47cde9d8c8de5bed30727b
ivey <iveysaurrr@gmail.com>
2014-08-03 20:11:54 -0400

    Spacing nitpicking

--- spinner.js
+++ spinner.js
@@ -7,14 +7,16 @@ window.addEventListener("load", function() {
 function stop() {
 	var ele = document.getElementById("spinner" + current);
 	var computed = window.getComputedStyle(ele);
+
 	if (computed) {
 		var split = computed.transform.split(", ");
 		var value = split[5].slice(0, -1);
-		var next = Math.round(value/70);
+		var next = Math.round(value / 70);
 		ele.style.webkitTransform = "translate(0, " + (next * 70) + "px)";
 		ele.className = "column"
 	}
 	current++;
+
 	if (current > 3) {
 		button.innerHTML = "Restart";
 		button.onclick = restart;
@@ -26,6 +28,7 @@ function restart() {
 		var ele = document.getElementById("spinner" + i);
 		ele.className = "column spinner"
 	}
+
 	current = 1;
 	button.innerHTML = "Stop";
 	button.onclick = stop;
